b41ce5891cf3eadd8558f0c067b27ee2c0947e99
Benjamin Schmitz <benjamin.schmitz@tum.de>
2022-06-04 08:37:25 +0200

    Fix error with SSH key permissions

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -2,7 +2,7 @@
 
 mkdir ~/.ssh
 echo "$SSH_KEY" > ~/.ssh/id_ed25519
-chmod 0600 /root/.ssh/id_ed25519
+chmod 0600 ~/.ssh/id_ed25519
 eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519
 
 cd ansible
